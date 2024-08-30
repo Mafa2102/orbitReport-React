3bac6aeb10ae581492d8c238417740350d6ebf57
Maria Usina <maryusina@Marias-Air.attlocal.net>
2024-08-30 12:52:25 -0500

    GA 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import Banner from "./components/Banner";
+import { useState } from "react";
+import satData from "./components/satData";
+import './App.css';
+import ImageSlider from "./components/ImageSlider";
+import { SliderData } from "./components/SliderData";
+
 
 
 function App() {
+
+  const [sat, setSat] = useState(satData);
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+  const filterByType = (currentType) => {
+    const displaySats = satData.filter((newSatDisplay) => {
+      return newSatDisplay.orbitType === currentType
+    });
+    setSat(displaySats);
+  };
   return (
-    <div>
-      <Buttons />
-      <Table />
-    </div>
+    <>
+      <Banner />
+      <Buttons
+        filterByType={filterByType}
+        setSat={setSat}
+        displaySats={displaySats}
+      />
+      <Table sat={sat} />
+      <ImageSlider slides={SliderData} />
+    </>
   );
 }
+
+
 
 export default App;
---
+++ src/components/Banner.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './styling.css'
+
+const Banner = () =>{
+    return(
+    <div class="box">
+        <header id='head'>Orbit Report</header>
+        <p>Click on the buttons to see the satellites in that orbit type</p>
+    </div>
+    )
+}
+
+export default Banner;
